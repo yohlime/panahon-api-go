be70c67c27d4f70aebaa801caa35a6e65fc77914
Emilio Gozo <15209351+yohlime@users.noreply.github.com>
2025-04-30 11:27:13 +0800

    use podman

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -3,4 +3,4 @@
 BASE_TAG="emiliogozo/panahon-api"
 DATE_STR="$(date +%Y%m%d)"
 
-docker build -t ${BASE_TAG} -t ${BASE_TAG}:"${DATE_STR}" .
+podman build -t ${BASE_TAG} -t ${BASE_TAG}:"${DATE_STR}" .
